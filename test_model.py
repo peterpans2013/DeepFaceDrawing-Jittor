bcf8f58712519ba2b06194ba38a2ffe771397963
peterpans2013 <31445699+peterpans2013@users.noreply.github.com>
2021-03-16 07:44:20 +0700

    fix output file name

--- test_model.py
+++ test_model.py
@@ -42,7 +42,7 @@ for x,fileName in enumerate(images_path):
     
     combine_model.predict_shadow(mat_img)
     
-    output_file = '/result/' + fileName +'.jpg'
+    output_file = '/result/' + fileName
     print('Output file:',output_file)
     cv2.imwrite(output_file,cv2.cvtColor(combine_model.generated, cv2.COLOR_BGR2RGB))
     i = i + 1
